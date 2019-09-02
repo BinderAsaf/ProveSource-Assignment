d282f92894263c1735fcce316dfb4ee5af3efd84
Asaf Binder <binderasaf@gmail.com>
2019-09-02 16:18:44 +0300

    Task 2 - handle duplicats email accounts
    - Changed the Account's model email field to be unique.
    - Check that email is given as a POST argument
      and return an error json if not
    - Insert the new account to the db and check for error.
      A json is sent on failure or success

--- api/account/create.js
+++ api/account/create.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const Account = require('../../models/account/Account');
 
+
+// @route    POST /account/create
+// @Info     creates new account
+// @params   email --REQUIRED, name, age 
 router.post('',async function(req, res) {
 	const {email, name, age} = req.body;
+	//check that an email is given
+	if (typeof email === 'undefined')
+		return res.send({err:"Email is required"});
+	
 	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+	// add the new account to db
+	await account.save()
+	.then(()=>{
+		return res.send({message: 'success'});
+	})
+	.catch(err =>{
+		// An error 11000 is when an email is duplicatted. email is unique!
+		if(err.code == 11000)
+			return res.send({"error": "Email already exists"});
+		else
+			return res.send({"error": "An error occurred"});		
+	});
+	
 });
 
 module.exports = router;
