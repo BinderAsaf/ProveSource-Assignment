59d25b39949db305e152c01a13b27b4d056c7610
Asaf Binder <binderasaf@gmail.com>
2019-09-02 15:52:41 +0300

    Task 1 - only accepts POST requests
    In order to do it i've changed the module to a router.
    Now this route of '/account/create' have only a POST option.
    Any other request to that route will cause 404 error.

--- api/account/create.js
+++ api/account/create.js
@@ -1,8 +1,12 @@
+const express = require('express');
+const router = express.Router();
 const Account = require('../../models/account/Account');
 
-module.exports = async function(req, res, next) {
+router.post('',async function(req, res) {
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
 	await account.save();
 	return res.send({message: 'success'});
-};
+});
+
+module.exports = router;
