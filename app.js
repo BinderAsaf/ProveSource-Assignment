af19358e148dab9aad30d2370f6376c3c1ca7d8f
Asaf Binder <binderasaf@gmail.com>
2019-09-02 15:44:00 +0300

    Setting up server and package.json
    - express.js
    - bodyParser setting: parse application/x-www-form-urlencoded
    - mongoose: mongodb connetion
    - routes

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const app = express();
 
+// db connection
 mongoose.connect('mongodb://localhost:27017/codeTest', {
 	autoReconnect: true,
 	reconnectTries: 60,
-	reconnectInterval: 10000
+	reconnectInterval: 10000,
+	useNewUrlParser: true,
+	useCreateIndex: true
 });
 
-const app = express();
-app.listen(3000);
 
-app.use(require('body-parser').json());
+//bodyParser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
+//routes uses
 app.use('/account/create', require('./api/account/create'));
 
+
+app.listen(3000);
 console.log('app running on port 3000...');
 
 module.exports = app;
